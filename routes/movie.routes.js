ea3beda4a1df794c1257d5aed637d63788352fd0
ujjawal396 <ujjawaljha08@gmail.com>
2026-02-17 20:47:39 +0530

    Added client side bad request checks for create movie using middlewares

--- routes/movie.routes.js
+++ routes/movie.routes.js
@@ -5,11 +5,13 @@
 
 
 
-const MovieController = require('../controllers/movie.controller');
+const MovieController = require('../controllers/movie.controller.js');
+const MovieMiddlewares =require('../middlewares/movie.middlewares.js')
 
 const routes = (app) => {
     // routes function takes express app object as parameter
-    app.post('/mba/api/v1/movies', MovieController.createMovie);
+    app.post('/mba/api/v1/movies', MovieMiddlewares.validateMovieCreateRequest,
+         MovieController.createMovie);
 }
 
 module.exports = routes;
