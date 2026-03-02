425605eededcd6641a4bfde173029035cf52f908
ujjawal396 <ujjawaljha08@gmail.com>
2026-03-02 22:42:36 +0530

    added theatre schema

---
+++ models/theatre.model.js
@@ -0,0 +1,33 @@
+const mongoose=require('mongoose');
+
+
+const theatreSchema=new mongoose.Schema({
+
+    name: {
+        type: String,
+        required: true
+    },
+    description: String,
+    city: {
+        type: String,
+        required: true
+    },
+    pincode: {
+        tpye: Number,
+        required: true
+    },
+    address: String
+
+
+
+
+
+},{timestamps:true});
+
+const Theatre=mongoose.model("Theatre",theatreSchema);
+
+module.exports={
+    theatre,
+
+}
+
