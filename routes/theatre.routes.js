a6d8c6754e1b66e24f7e181dc418876189a23c9e
ujjawal396 <ujjawaljha08@gmail.com>
2026-03-02 23:52:20 +0530

    theatre middlewares validators api added

--- routes/theatre.routes.js
+++ routes/theatre.routes.js
@@ -1,7 +1,10 @@
 const theatreController=require('../controllers/theatre.controller.js');
+const theatreMiddleware=require('../middlewares/theatre.middlewares.js');
+
 
 const routes=(app)=>{
     app.post('mba/api/v1/theatres',
+        theatreMiddleware.validateTheatreCreateRequest,
         theatreController.create);
 }
 
