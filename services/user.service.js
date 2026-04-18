e9c42e8712a57f8faf4e6337c98249e9799f875d
ujjawal396 <ujjawaljha08@gmail.com>
2026-04-19 00:17:16 +0530

    Added basic middlewares to auth

--- services/user.service.js
+++ services/user.service.js
@@ -5,7 +5,15 @@ const createUser = async (data) => {
         const response = await User.create(data);
         return response;
     } catch (error) {
-        console.log(error);
+
+         if(error.name == 'ValidationError') {
+            let err = {};
+            Object.keys(error.errors).forEach((key) => {
+                err[key] = error.errors[key].message;
+            });
+            throw {err: err, code: 422};
+        }
+        
         throw error;
     }
 }
