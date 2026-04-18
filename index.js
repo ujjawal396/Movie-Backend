b2796826d5541036c24b11684919f34d923a4c3f
ujjawal396 <ujjawaljha08@gmail.com>
2026-04-19 00:44:53 +0530

    Added update user role or status api

--- index.js
+++ index.js
@@ -17,9 +17,12 @@ mongoose.set('debug', true);
 const MovieRoutes = require('./routes/movie.routes.js');
 const theatreRoutes = require('./routes/theatre.routes.js');
 const authRoutes = require('./routes/auth.routes');
+const userRoutes = require('./routes/user.routes');
 MovieRoutes(app); // invoking movie routes
 theatreRoutes(app); // involing theatre routes
 authRoutes(app); // invoking auth routes
+userRoutes(app); // invoking user routes
+
 
 app.get('/',(req,res)=>{
     
