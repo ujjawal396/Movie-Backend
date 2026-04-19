018f2a2736f6c8f6c1d8d2402f44e945ded15961
ujjawal396 <ujjawaljha08@gmail.com>
2026-04-20 00:37:27 +0530

    Added booking schema with mongoose

---
+++ models/booking.model.js
@@ -0,0 +1,47 @@
+const mongooose=require('mongoose');
+
+const { BOOKING_STATUS } = require('../utils/constants');
+
+const bookingSchema =new mongooose.Schema({
+
+theatreId: {
+        type: mongoose.Schema.Types.ObjectId,
+        required: true,
+        ref: 'Theatre'
+    },
+    movieId: {
+        type: mongoose.Schema.Types.ObjectId,
+        required: true,
+        ref: 'Movie'
+    },
+    userId: {
+        type: mongoose.Schema.Types.ObjectId,
+        required: true,
+        ref: 'User'
+    },
+    timing: {
+        type: String,
+        required: true
+    },
+    noOfSeats: {
+        type: Number,
+        required: true,
+    },
+    totalCost: {
+        type: Number
+    },
+    status: {
+        type: String,
+        required: true,
+        enum: {
+            values: [BOOKING_STATUS.processing, BOOKING_STATUS.cancelled, BOOKING_STATUS.successfull],
+            message: "Invalid booking status"
+        },
+        default: BOOKING_STATUS.processing
+    }
+}, {timestamps: true});
+
+
+const Booking=new mongooose.model("Booking",bookingSchema);
+
+module.exports = Booking;
--- utils/constants.js
+++ utils/constants.js
@@ -21,8 +21,16 @@ const STATUS_CODES = {
       UNPROCESSABLE_ENTITY: 422
 }
 
+const BOOKING_STATUS = {
+    cancelled: "CANCELLED",
+    successfull: "SUCCESSFULL",
+    processing: "IN_PROCESS"
+}
+
+
 module.exports = {
     USER_ROLE,
     USER_STATUS,
-    STATUS_CODES,
+   STATUS: STATUS_CODES,
+    BOOKING_STATUS,
 }
